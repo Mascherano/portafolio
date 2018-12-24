a3cfa44732c5532bf3acf23574091fc0fcf9264c
MarceloOctopass <marcelogonzalez@octopass.cl>
2018-12-24 00:39:45 -0300

    creación de un front controller para manejar en un unico archivo todas las peticiones a nuestra aplicación además se instala un router que cumple con los estandares de psr-7

---
+++ public/index.php
@@ -0,0 +1,53 @@
+<?php
+
+	ini_set('display_errors', 1);
+	ini_set('display_starup_error', 1);
+	error_reporting(E_ALL);
+
+	require_once '../vendor/autoload.php';
+
+	use Illuminate\Database\Capsule\Manager as Capsule;
+	use Aura\Router\RouterContainer;
+
+	$capsule = new Capsule;
+
+	$capsule->addConnection([
+	    'driver'    => 'mysql',
+	    'host'      => 'localhost',
+	    'database'  => 'portafolio',
+	    'username'  => 'root',
+	    'password'  => '',
+	    'charset'   => 'utf8',
+	    'collation' => 'utf8_unicode_ci',
+	    'prefix'    => '',
+	]);
+
+	// Make this Capsule instance available globally via static methods... (optional)
+	$capsule->setAsGlobal();
+
+	// Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
+	$capsule->bootEloquent();
+
+	$request = Zend\Diactoros\ServerRequestFactory::fromGlobals(
+	    $_SERVER,
+	    $_GET,
+	    $_POST,
+	    $_COOKIE,
+	    $_FILES
+	);
+
+	$routerContainer = new RouterContainer();
+	$map = $routerContainer->getMap();
+	$map->get('index', '/portafolio/', '../index.php');
+	$map->get('addJobs', '/portafolio/jobs/add', '../addJob.php');
+
+	$matcher = $routerContainer->getMatcher();
+	$route = $matcher->match($request);
+
+	if(!$route){
+		echo 'No route';
+	}else{
+		require $route->handler;
+	}
+	
+
